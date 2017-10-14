9cfb3c0a4ce7f9b127637fa78a4da95451418d3b
Nihal Ismail K <nihalismailk@gmail.com>
2017-10-14 18:27:36 +0530

    Data encoded

--- ANN.py
+++ ANN.py
@@ -9,5 +9,16 @@ import numpy as np
 import pandas as pd
 
 #!!!-Data Preprocessing-!!!
-dataset=pd.read_csv("Churn_Modelling")
 
+#Importing the dataset
+dataset=pd.read_csv("Churn_Modelling.csv")
+x=dataset.iloc[:,3:-1].values
+y=dataset.iloc[:,-1].values
+
+#Encoding the data
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelEncoder=LabelEncoder()
+x[:,1]=labelEncoder.fit_transform(x[:,1])
+x[:,2]=labelEncoder.fit_transform(x[:,2])
+oneHotEncoder=OneHotEncoder(categorical_features=[1],dtype=np.int)
+x=oneHotEncoder.fit_transform(x).toarray()
