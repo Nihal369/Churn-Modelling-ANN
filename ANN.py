dd6573c02fdc1f9fa0fc90725ffd9c2d43af9d74
Nihal Ismail K <nihalismailk@gmail.com>
2017-10-14 18:20:59 +0530

    Imported the dataset

---
+++ ANN.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 14 18:17:12 2017
+
+@author: nihal369
+"""
+#Importing libraries
+import numpy as np
+import pandas as pd
+
+#!!!-Data Preprocessing-!!!
+dataset=pd.read_csv("Churn_Modelling")
+
